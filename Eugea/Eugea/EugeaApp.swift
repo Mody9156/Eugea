ffbbc65f25bb5ea527f24d57d31a3b270b4ddb4f
KEITA Modibo <Modykeita2@gmail.com>
2025-12-19 18:34:32 +0100

    nouveau projet

---
+++ Eugea/Eugea/EugeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EugeaApp.swift
+//  Eugea
+//
+//  Created by Modibo on 19/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct EugeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
