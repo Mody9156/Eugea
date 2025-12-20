62adf266e355767825a13a7439c7468dc8e8fd04
KEITA Modibo <Modykeita2@gmail.com>
2025-12-20 16:23:29 +0100

    modification du text

--- Eugea/Eugea/View/HomeView.swift
+++ Eugea/Eugea/View/HomeView.swift
@@ -11,7 +11,16 @@ struct HomeView: View {
     @State var name : String = "Joe"
     var body: some View {
         VStack {
-            Text("Bonjour,\(name)")
+            Text("Bonjour, \(name)")
+                .font(.system(size: 24, weight: .medium))
+                .lineSpacing(8)
+
+            Text("Comment vous sentez-vous aujourd'hui ?")
+                .font(.system(size: 16, weight: .regular))
+                .foregroundStyle(.secondary)
+                .lineSpacing(8)
+
+
         }
         .padding()
     }
