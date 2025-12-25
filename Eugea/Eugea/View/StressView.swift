6f469e16f3e88e9e8ee5f1a102fe7da16cc3a9b2
KEITA Modibo <Modykeita2@gmail.com>
2025-12-25 11:15:00 +0100

    nouvelle slider

--- Eugea/Eugea/View/StressView.swift
+++ Eugea/Eugea/View/StressView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct StressView: View {
+    @State private var speed = 50.0
+    @State private var isEditing = false
     @State var stressLevel: Int = 0
     @State var activityName : String = ""
     
     var body: some View {
         VStack{
-            
+            Slider(value: $speed, in: 0...10) { editing in
+                isEditing = editing
+            }
+            Text("\(Double(speed))")
         }
     }
 }
