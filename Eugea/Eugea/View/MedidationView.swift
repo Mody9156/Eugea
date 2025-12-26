f5c841b979778828326ade2f6cf996e25e1eddf8
KEITA Modibo <Modykeita2@gmail.com>
2025-12-26 16:19:05 +0100

    nouvelle vue

---
+++ Eugea/Eugea/View/MedidationView.swift
@@ -0,0 +1,18 @@
+//
+//  MedidationView.swift
+//  Eugea
+//
+//  Created by Modibo on 26/12/2025.
+//
+
+import SwiftUI
+
+struct MedidationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MedidationView()
+}
