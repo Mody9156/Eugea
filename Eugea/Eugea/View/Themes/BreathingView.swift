87e381204074992107ab51bda618e3876ea841ce
KEITA Modibo <Modykeita2@gmail.com>
2025-12-27 20:11:00 +0100

    nouveau VideoPlayer

--- Eugea/Eugea/View/Themes/BreathingView.swift
+++ Eugea/Eugea/View/Themes/BreathingView.swift
@@ -13,7 +13,27 @@ struct BreathingView: View {
     @State private var isPlaying = false
     
     var body: some View {
-        Text("Respiration")
+        VStack {
+            if let player {
+                VideoPlayer(player: player)
+                    .frame(width: 320, height: 180, alignment: .center)
+                
+                Button {
+                    isPlaying ? player.pause() : player.play()
+                    isPlaying.toggle()
+                    player.seek(to: .zero)
+                } label: {
+                    Image(systemName: isPlaying ? "stop" : "play")
+                        .padding()
+                }
+            }
+        }
+        .task {
+            // Use the task modifier to defer creating the player to ensure
+            // SwiftUI creates it only once when it first presents the view.
+            let url = URL(string: "https://yoursite.com/video.mp4")!
+            player = AVPlayer(url: url)
+        }
     }
 }
 
