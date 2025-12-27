ac66157051adbd5c08ee9e6a6d0ec112677874d7
KEITA Modibo <Modykeita2@gmail.com>
2025-12-27 19:59:22 +0100

    nouvelle vue

---
+++ Eugea/Eugea/View/Themes/BreathingView.swift
@@ -0,0 +1,18 @@
+//
+//  BreathingView.swift
+//  Eugea
+//
+//  Created by Modibo on 27/12/2025.
+//
+
+import SwiftUI
+
+struct BreathingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BreathingView()
+}
