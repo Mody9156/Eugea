62e91b9013be4109f3d0727b8e5bc55a9010c9dc
KEITA Modibo <Modykeita2@gmail.com>
2025-12-31 21:02:59 +0100

    initialisation du fichier

--- Eugea/Eugea/ViewModel/antiAnxietyViewViewModel.swift
+++ Eugea/Eugea/ViewModel/antiAnxietyViewViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Observation
+
+@Observable
+class antiAnxietyViewModel {
+        let meditationConfiguration: MeditationConfiguration
+        var MeditationType : [MeditationType] = []
+    
+        init(meditationConfiguration: MeditationConfiguration = MeditationConfiguration()) {
+            self.meditationConfiguration = meditationConfiguration
+        }
+    
+        enum ThrowableError: Swift.Error {
+            case someError
+        }
+    
+        func showExercise() async throws {
+            do{
+                let result =  try await meditationConfiguration.fetchResult_ofMeditation()
+                self.MeditationType = [result]
+            }catch{
+                throw ThrowableError.someError
+            }
+        }
+}
