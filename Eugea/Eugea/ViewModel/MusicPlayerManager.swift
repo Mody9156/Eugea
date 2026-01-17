6bb24e1f7c18625b6049092c4a18c99f648c097f
KEITA Modibo <Modykeita2@gmail.com>
2026-01-17 21:29:00 +0100

    nouvelle function

--- Eugea/Eugea/ViewModel/MusicPlayerManager.swift
+++ Eugea/Eugea/ViewModel/MusicPlayerManager.swift
@@ -6,7 +6,18 @@
 //
 
 import Foundation
-
+import Observation
+import AVFoundation
 class MusicPlayerManager {
+    static var share = MusicPlayerManager()
+    private var player : AVPlayer?
+    var isPlaying : Bool = false
     
+    
+    func playSong(song:String) {
+//        let playerItem = AVPlayer
+//        player = AVPlayer(playerItem: AVPlayerItem()
+        player?.play()
+        isPlaying = true
+    }
 }
