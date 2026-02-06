eca8199d07d5a6815e6517a6c5e1cb249d995342
KEITA Modibo <Modykeita2@gmail.com>
2026-02-06 15:55:31 +0100

    nouvelle vue

---
+++ Eugea/Eugea/View/Meditation_themes/Anxiety/AddExercise.swift
@@ -0,0 +1,18 @@
+//
+//  AddExercise.swift
+//  Eugea
+//
+//  Created by Modibo on 06/02/2026.
+//
+
+import SwiftUI
+
+struct AddExercise: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExercise()
+}
